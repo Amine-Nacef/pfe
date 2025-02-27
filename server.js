4b7630502e4f6533164bbb11ff033b83ab30ed74
Amine-Nacef <amine.nacef6@gmail.com>
2025-02-27 16:26:19 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express=require("express");
 const app=express();
 app.use(express.urlencoded({ extended: false }));
 //app.use(express.json); le programme mamchach avec
+app.set('view engine', 'ejs');
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -17,7 +18,7 @@ var con = mysql.createConnection({
     if (err) throw err;
     console.log('db ' + con.state);
   });
-
+/* AJT_USER : */
 app.post("/ajouterutilisateur",(req,res)=>{
     const {nom,prenom,email,mot_de_passe,role}=req.body;
 
@@ -73,38 +74,14 @@ app.post("/ajouterfuite",(req,res)=>{
 }});
 });
 
-app.get("/extrairefuite",(req,res)=>{
-  id=req.query.id;
-  con.query("SELECT * FROM fuite WHERE id= ?;",[id], function (err, result) {
+app.get("/",(req,res)=>{
+  con.query("SELECT * FROM fuite", function (err, result) {
     if (err) {
         return res.status(500).send("la fuite n'a pas pu etre extrait");
       }
-    res.json(result[0]);
+    res.render('index', { items: result });
   });
 })
 app.listen(3000,()=>{
-  console.log("server is running on port 3000");
-});
-app.get("/extrairefuite_ids", (req, res) => {
-  con.query("SELECT id FROM fuite", (err, results) => {
-      if (err) {
-          return res.status(500).send("Erreur lors de l'extraction des IDs");
-      }
-      const ids = results.map(row => row.id);
-      res.json(ids);
-  });
-});
-app.get("/extrairefuite", (req, res) => {
-  const id = req.query.id;
-  if (!id) return res.status(400).send("ID manquant");
-
-  con.query("SELECT information, lien, source, date FROM fuite WHERE id = ?;", [id], (err, result) => {
-      if (err) {
-          return res.status(500).send("Erreur lors de l'extraction de la fuite");
-      }
-      if (result.length === 0) {
-          return res.status(404).send("Aucune donnée trouvée pour cet ID");
-      }
-      res.json(result[0]);
-  });
+  console.log("Server running on http://localhost:3000");
 });
